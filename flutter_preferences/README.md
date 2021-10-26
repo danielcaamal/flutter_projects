490d82e05783616604e476ffcc2ba160c9b1ed10
danielcaamal <danielcaamal97@gmail.com>
2021-10-25 21:15:22 -0500

    Finalización del proyecto

--- flutter_preferences/README.md
+++ flutter_preferences/README.md
@@ -4,16 +4,11 @@ A new Flutter project.
 
 - Drawers
 - Preferences
+- initState
+- onDestroy
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+<p align="center">
+    <img src="./user_preferences.png"
+        alt="Example Flutter app"
+        style="float: left; margin-right: 10px; height: 500px"/>
+</p>
--- flutter_preferences/lib/src/pages/settings_page.dart
+++ flutter_preferences/lib/src/pages/settings_page.dart
@@ -1,18 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_preferences/src/share_prefs/user_preferences.dart';
+import 'package:flutter_preferences/src/widgets/menu_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static const String routeName = 'Settings';
 
   const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  final prefs = UserPreferences();
+  bool _secondaryColor = false;
+  int _gender = 1;
+  String _name = 'Jean';
+
+  TextEditingController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _gender = prefs.gender;
+    _secondaryColor = prefs.secondaryColor;
+    _name = prefs.username;
+    prefs.lastPage = SettingsPage.routeName;
+    _controller = TextEditingController(text: _name);
+  }
+
+  void _setSelectedRadio(int? value) {
+    if (value != null) {
+      prefs.gender = value;
+      _gender = value;
+    }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Settings'),
+          backgroundColor: (prefs.secondaryColor) ? Colors.teal : Colors.blue,
         ),
-        body: Center(
-          child: Text('Settings'),
+        drawer: const MenuWidget(),
+        body: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(5.0),
+              child: const Text(
+                'Settings',
+                style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Divider(),
+            SwitchListTile(
+                value: _secondaryColor,
+                title: const Text('Secondary Color'),
+                onChanged: (value) {
+                  _secondaryColor = value;
+                  prefs.secondaryColor = value;
+                  setState(() {});
+                }),
+            RadioListTile(
+                value: 1,
+                title: const Text('Male'),
+                groupValue: _gender,
+                onChanged: _setSelectedRadio),
+            RadioListTile(
+                value: 2,
+                title: const Text('Female'),
+                groupValue: _gender,
+                onChanged: _setSelectedRadio),
+            const Divider(),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    helperText: 'Username',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 0.0),
+                    )),
+                onChanged: (value) {
+                  prefs.username = value;
+                },
+              ),
+            )
+          ],
         ));
   }
 }
