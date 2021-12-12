e60064cd44e8d711043f7c3fce9a8898e42ff678
daniel.caamal <danielcaamal97@gmail.com>
2021-12-12 15:15:50 -0600

    Se agrega el product form provider

--- flutter_products_app/lib/main.dart
+++ flutter_products_app/lib/main.dart
@@ -16,12 +16,14 @@ class AppState extends StatelessWidget {
           create: (_) => ProductsService(),
         )
       ],
-      child: MyApp(),
+      child: const MyApp(),
     );
   }
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- flutter_products_app/lib/screens/product_screen.dart
+++ flutter_products_app/lib/screens/product_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_products_app/providers/product_form_provider.dart';
+import 'package:flutter_products_app/services/services.dart';
 import 'package:flutter_products_app/ui/input_decorations.dart';
 import 'package:flutter_products_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class ProductScreen extends StatelessWidget {
   static const String routeName = 'Product';
@@ -8,13 +12,37 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productService = Provider.of<ProductsService>(context);
+    // print(productService.selectedProduct.name);
+
+    return ChangeNotifierProvider(
+      create: (_) => ProductFormProvider(productService.selectedProduct),
+      child: _ProductsScreenBody(productService: productService),
+    );
+    // return
+  }
+}
+
+class _ProductsScreenBody extends StatelessWidget {
+  const _ProductsScreenBody({
+    Key? key,
+    required this.productService,
+  }) : super(key: key);
+
+  final ProductsService productService;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
+        // keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         child: Column(
           children: [
             Stack(
               children: [
-                const ProductImage(),
+                ProductImage(
+                  url: productService.selectedProduct.picture,
+                ),
                 Positioned(
                     top: 40,
                     left: 20,
@@ -60,6 +88,9 @@ class _ProductForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: Container(
@@ -75,6 +106,13 @@ class _ProductForm extends StatelessWidget {
               TextFormField(
                 decoration: InputDecorations.authInputDecoration(
                     hintText: 'Product name', labelText: 'Name: '),
+                initialValue: product.name,
+                onChanged: (value) => product.name = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El nombre es obligatorio';
+                  }
+                },
               ),
               const SizedBox(
                 height: 30,
@@ -83,6 +121,13 @@ class _ProductForm extends StatelessWidget {
                 keyboardType: TextInputType.number,
                 decoration: InputDecorations.authInputDecoration(
                     hintText: '\$150', labelText: 'Price: '),
+                initialValue: '${product.price}',
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(
+                      RegExp(r'^(\d+)?\.?\d{0,2}'))
+                ],
+                onChanged: (value) =>
+                    product.price = double.tryParse(value) ?? 0,
               ),
               const SizedBox(
                 height: 10,
@@ -90,8 +135,8 @@ class _ProductForm extends StatelessWidget {
               SwitchListTile(
                   title: const Text('Available'),
                   activeColor: Colors.indigo,
-                  value: true,
-                  onChanged: (value) {}),
+                  value: product.available,
+                  onChanged: productForm.updateAvailability),
               const SizedBox(
                 height: 10,
               ),
